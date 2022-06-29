d6e168fb29addd8ab08860abd51a675a59b45172
sahildua899 <sahil.hsim@gmail.com>
2022-06-29 12:55:07 +0530

    some changes

--- js/main.js
+++ js/main.js
@@ -221,3 +221,19 @@ startImageTransition();
                 })
             }
         }   
+
+        // Big Blog Slider
+
+        var swiper = new Swiper('.blog-slider', {
+          spaceBetween: 30,
+          effect: 'fade',
+          loop: true,
+          mousewheel: {
+            invert: false,
+          },
+          // autoHeight: true,
+          pagination: {
+            el: '.blog-slider__pagination',
+            clickable: true,
+          }
+        });
